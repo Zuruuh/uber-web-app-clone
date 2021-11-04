285cb0a689e93f9ffb84a43f2af3489756b358aa
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-11-05 00:12:14 +0100

    ✨ Created register hook

---
+++ src/hooks/auth/useRegister.tsx
@@ -0,0 +1,60 @@
+import firebaseClient from '../../auth/firebase';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import type { UserCredential } from 'firebase/auth';
+import type { AuthResponse, RegisterInterface, AuthError } from '../../types';
+
+export const useRegister = async ({
+  email,
+  password,
+  repeatPassword,
+}: RegisterInterface): Promise<AuthResponse> => {
+  if (password !== repeatPassword) {
+    return {
+      error: {
+        field: 'repeatPassword',
+        message: 'Passwords does not match.',
+      },
+    };
+  }
+  firebaseClient();
+
+  let authError: AuthError;
+  const res: UserCredential | void = await createUserWithEmailAndPassword(
+    getAuth(),
+    email,
+    password,
+  ).catch(({ message }) => {
+    console.log(message);
+    let error: AuthError;
+    switch (true) {
+      case message.includes('auth/invalid-email'):
+        error = {
+          field: 'email',
+          message: 'Please enter a valid email address.',
+        };
+        break;
+      case message.includes('auth/weak-password'):
+        error = {
+          field: 'password',
+          message: 'Your password should be at least 6 characters.',
+        };
+        break;
+      case message.includes('auth/email-already-in-use'):
+        error = {
+          field: 'password',
+          message: 'This email is already in use.',
+        };
+      default:
+        console.log(message);
+        break;
+    }
+    authError = error;
+  });
+  if (authError) {
+    return { error: authError };
+  }
+  if (res) {
+    return { credentials: res };
+  }
+  console.error('Unexpected error');
+};
